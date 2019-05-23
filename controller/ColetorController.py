3a553cb35e6bf2cd49a78d080cfaaf1e829b3b21
Ana Renault <anaelisarenault@hotmail.comm>
2019-05-23 20:11:48 -0300

    controller changes

--- controller/ColetorController.py
+++ controller/ColetorController.py
@@ -1,21 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify 
+import requests 
+from bs4 import BeautifulSoup 
 
 app = Flask (__name__) 
-@app.route ("/coleta01") 
-def iniciar_GET_implicito (): 
-  return jsonify ({"resp": "Coletor iniciado através do método GET implicitamente."}) 
-#o atributo methods de routes() especifica o método do protocolo HTTP 
-@app.route ("/coleta02", methods=['GET']) 
-def iniciar_GET_explicito (): 
-  return jsonify ({"resp": "Coletor iniciado através do método GET explicitamente."}) 
-  
-@app.route ("/coleta03", methods=['POST']) 
-def iniciar_POST (): 
-  return jsonify ({"resp": "Coletor iniciado através do método POST."}) 
-  
-@app.route ("/coleta04", methods=['GET', 'POST']) 
-def iniciar_GET_POST (): 
-  return jsonify ({"resp": "Coletor inciado através do método GET ou POST."}) 
-  
+
+@app.route ("/paginaHTML")
+def paginaHTML (): 
+  url = "http://journals.ecs.soton.ac.uk/java/tutorial/networking/urls/readingWriting.html" 
+  requisicao = requests.get (url) 
+  print ("Código HTTP de resposta: " + str (requisicao.status_code)) 
+  pagina = requisicao.text 
+  return pagina @app.route ("/paginaTXT") 
+
+def paginaTXT (): 
+  url = "http://journals.ecs.soton.ac.uk/java/tutorial/networking/urls/readingWriting.html" 
+  requisicao = requests.get (url) 
+  print ("Código HTTP de resposta: " + str (requisicao.status_code)) 
+  pagina = requisicao.text
+  soup = BeautifulSoup (pagina) 
+  txt = soup.get_text () 
+  return txt 
+
 if __name__ == "__main__": 
   app.run ()
